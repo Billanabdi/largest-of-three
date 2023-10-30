255358b5cdd1aa7b48f73ccb87e4407ed3748b6b
Billan Abdi <billanabdi04@gmail.com>
2023-10-30 16:12:08 +0400

    Answers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const x = 100;
+const y = 200;
+const z = 300;
+
+if (x < z && y < z) {
+  console.log(`The larger of the three is ${z}`);
+}
